c86d79c6f8dff944752450e2cff522f02fd2efa6
Xuanzhiwang <610601617@qq.com>
2022-10-13 12:50:26 +0800

    Complete a hello program in C++.

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion: ***
+ * @Author: Harlam
+ * @Date: 2022-10-13 12:49:09
+ * @LastEditors: Harlam
+ * @LastEditTime: 2022-10-13 12:50:11
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    cout<<"Hello ,B511!"<<endl;
+    return 0;
+}
